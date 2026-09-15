b1c0d7841c772069077b4d1c2d3bea0369789bb0
Vassilios Moulakakis <68458826+vmoulakakis@users.noreply.github.com>
2026-09-15 14:16:32 +0300

    feat(v36): activate bidirectional funnel on Greek home

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import { V34EscapeFunnel } from "@/components/v34-escape-funnel";
+import { V36EscapeFunnel } from "@/components/v36-escape-funnel";
 
 export const dynamic="force-dynamic";
 
 export default function HomePage(){
- return <V34EscapeFunnel lang="el"/>;
+ return <V36EscapeFunnel lang="el"/>;
 }
